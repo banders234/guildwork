d6ebe3954b1d189088ba4a7f45d2bfee423a29c2
Brandon Anderson <Brandon Anderson>
2016-04-19 19:59:20 -0400

    BASW-7 - Committed PBD13

---
+++ PBD/YourSchedule/src/main/java/com/mycompany/yourschedule/YourSchedule.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package yourschedule;
+
+/**
+ *
+ * @author bande
+ */
+public class YourSchedule {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       String course1 = "Java";
+       String course2 = "Git";
+       String course3 = "Music Theory";
+       String course4 = "Biotechnology";
+       String course5 = "Principles of Technology I";
+       String course6 = "Latin II";
+       String course7 = "AP US History";
+       String course8 = ".net";
+       String teacher1 = "Mr. Toner";
+       String teacher2 = "Mr. Pudelski";
+       String teacher3 = "Mr. Davis";
+       String teacher4 = "Ms. Palmer";
+       String teacher5 = "Ms. Garcia";
+       String teacher6 = "Mrs. Barnett";
+       String teacher7 = "Ms. Johannessen";
+       String teacher8 = "Mr. Balzer";
+       System.out.println("+--------------------------------------------------+");
+       System.out.println("|1|                           "+ course1+"|       "+ teacher1 + "|");
+       System.out.println("|2|                            "+ course2+"|    "+ teacher2 + "|");
+       System.out.println("|3|                   "+ course3+"|       "+ teacher3 + "|");
+       System.out.println("|4|                  "+ course4+"|      "+ teacher4 + "|");
+       System.out.println("|5|     "+ course5+"|      "+ teacher5 + "|");
+       System.out.println("|6|                       "+ course6+"|    "+ teacher6 + "|");
+       System.out.println("|7|                  "+ course7+"| "+ teacher7 + "|");
+       System.out.println("|8|                           "+ course8+"|      "+ teacher8 + "|");
+       System.out.println("+--------------------------------------------------+");
+    }
+    
+}
