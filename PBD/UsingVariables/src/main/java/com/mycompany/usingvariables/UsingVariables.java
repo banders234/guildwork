acf0e06765c922d0b0d4f5b1fc4ea04febe4540b
Brandon Anderson <banders234@yahoo.com>
2016-04-19 20:55:13 -0400

    BASW-5 - Committed PBD11

---
+++ PBD/UsingVariables/src/main/java/com/mycompany/usingvariables/UsingVariables.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.usingvariables;
+
+/**
+ *
+ * @author apprentice
+ */
+public class UsingVariables {
+    public static void main(String[] args) {
+        int room = 113;
+        double e = 2.71828;
+        String learn = "Computer Science";
+        System.out.println("This is room # " + room);
+        System.out.println("e is close to " + e);
+        System.out.println("I am learning a bit about " + learn);
+        
+    }
+}
