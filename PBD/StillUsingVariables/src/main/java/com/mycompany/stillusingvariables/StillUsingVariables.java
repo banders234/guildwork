c90afc28364ce6381fdddf38a124ded7e5bcd95e
Brandon Anderson <banders234@yahoo.com>
2016-04-19 20:35:54 -0400

    BASW-6 - Committed PBD12

---
+++ PBD/StillUsingVariables/src/main/java/com/mycompany/stillusingvariables/StillUsingVariables.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.stillusingvariables;
+
+/**
+ *
+ * @author apprentice
+ */
+public class StillUsingVariables {
+    public static void main(String[] args) {
+        String name = "Brandon Anderson";
+        int year = 2016;
+        System.out.println("My name is " + name + " and I'll graduate in " + year);
+    }
+}
