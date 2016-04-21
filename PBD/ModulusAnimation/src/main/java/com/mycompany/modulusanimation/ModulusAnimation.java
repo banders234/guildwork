c5ce443b98a1e9a8fcea18fe882c4a69d73c6da0
Brandon Anderson <banders234@yahoo.com>
2016-04-20 20:45:36 -0400

    BASW-22 - Added Modulus Animation

---
+++ PBD/ModulusAnimation/src/main/java/com/mycompany/modulusanimation/ModulusAnimation.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.modulusanimation;
+
+/**
+ *
+ * @author apprentice
+ */
+public class ModulusAnimation
+{
+        public static void main ( String[] args ) throws Exception
+        {
+            for ( int i=0; i<80; i++ )
+            {
+                    if ( i%10 == 0 )
+                        System.out.print("______\r");
+                    else if ( i%10 == 1 )
+                        System.out.print("|     |\r");
+                    else if ( i%10 == 2 )
+			System.out.print("|     |\r");
+                    else if ( i%10 == 3 )
+			System.out.print("|     |\r");
+                    else if ( i%10 == 4 )
+			System.out.print("|     |\r");
+                    else if ( i%10 == 5 )
+			System.out.print("|  *  |\r");
+                    else if ( i%10 == 6 )
+			System.out.print("|     |\r");
+                    else if ( i%10 == 7 )
+			System.out.print("|     |\r");
+                    else if ( i%10 == 8 )
+			System.out.print("|     |\r");
+                    else if ( i%10 == 9 )
+			System.out.print("______\r");
+                    Thread.sleep(200);
+		}
+		
+	}
+}
+
